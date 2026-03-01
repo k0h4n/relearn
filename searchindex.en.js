f8cbcc4c9a0f5df012740f38bb9ca1aa23aef8d5
k0h4n <k0h4n@users.noreply.github.com>
2026-03-01 23:04:17 +0000

    deploy: b2bf677285eed66f70b17a2ef76669c1cc79605b

--- searchindex.en.js
+++ searchindex.en.js
@@ -8,12 +8,52 @@ var relearn_searchindex = [
     "uri": "/trading/index.html"
   },
   {
+    "breadcrumb": "Relearn Everything \u003e Youtubes \u003e Doctor Mike Diamonds",
+    "content": "Summary from Doctor Mike Diamonds video titled 5 Easy Steps To Get To 12% Body Fat (At Any Age)\n5 Golden Rule Walking Start slowly at 8000 steps Increase gradually to 15000 steps Recommended app Pedometer++ Proteios (Protein) Recommended 1 g per 1 pound (lbs) of body weight Rough estimation = 1 palm sized Chicken or steak = 25 g of protein 2 6 8 10 = 50 g of protein (ounce = 28.35 g) 2 Scoop of protein powder 6 oz of Chicken breast = 170.1 g 8 oz of beef = 226.8 g 10 oz of white fish = 283.5 g Elixir of life Train with good form Intermitten Fasting fasting to reset your metabolism Sleep At least sleep 7 hours Sleepng routine 10 3 2 1: 10 Hours before bed no more caffeine 3 Hours before bed no more food 2 Hours before bed no more drink 1 Hour before bed no blue light",
+    "description": "Summary from Doctor Mike Diamonds video titled 5 Easy Steps To Get To 12% Body Fat (At Any Age)\n5 Golden Rule Walking Start slowly at 8000 steps Increase gradually to 15000 steps Recommended app Pedometer++ Proteios (Protein) Recommended 1 g per 1 pound (lbs) of body weight Rough estimation = 1 palm sized Chicken or steak = 25 g of protein 2 6 8 10 = 50 g of protein (ounce = 28.35 g) 2 Scoop of protein powder 6 oz of Chicken breast = 170.1 g 8 oz of beef = 226.8 g 10 oz of white fish = 283.5 g Elixir of life Train with good form Intermitten Fasting fasting to reset your metabolism Sleep At least sleep 7 hours Sleepng routine 10 3 2 1: 10 Hours before bed no more caffeine 3 Hours before bed no more food 2 Hours before bed no more drink 1 Hour before bed no blue light",
+    "tags": [],
+    "title": "5 Easy Steps To Get To 12% Body Fat (At Any Age)",
+    "uri": "/youtube/doctormikediamonds/12bodyfat/index.html"
+  },
+  {
+    "breadcrumb": "Relearn Everything \u003e Youtubes",
+    "content": "Summary yang di buat dari channel Doctor Mike Diamonds",
+    "description": "Summary yang di buat dari channel Doctor Mike Diamonds",
+    "tags": [],
+    "title": "Doctor Mike Diamonds",
+    "uri": "/youtube/doctormikediamonds/index.html"
+  },
+  {
     "breadcrumb": "",
     "content": "This site is dedicated to common knowledge I capture for my future reference.",
     "description": "This site is dedicated to common knowledge I capture for my future reference.",
     "tags": [],
     "title": "Relearn Everything",
     "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Relearn Everything",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Youtubes",
+    "uri": "/youtube/index.html"
+  },
+  {
+    "breadcrumb": "Relearn Everything \u003e Youtubes \u003e Bennix",
+    "content": "Summary dari video Bennix yang berjudul 5 Rahasia Disiplin Samurai Jepang untuk Mengalahkan Malas!\nDisiplin lebih penting dari pada motivasi\nFokus membangun disiplin yang benar bukan menunggu semangat itu datang\nKalau menunggu motivasi kita akan kalah di medan perang, karena perang bisa datang kapan saja\n5 Prinsip dasar Samurai Kodo Ga Kokoro O Tsukuru (Tindakan membentuk perasaan) Bertindak tanpa mengandalkan mood Bertindak itu harus di lakukan karena itu memang harus dilakukan tanpa menunggu mood Dalam filosofi samurai, perasaan itu tidak boleh di andalkan dan di percaya Karena perasaan itu labil dan bisa berubah ubah (bisa naik turun) Kaizen - Lakukan sesuatu yang kecil tapi rutin setiap hari Lakukan perbaikan 1% terus menerus Seperti menabung, sedikit sedikit lama lama menjadi bukit (coumpounding) Progress itu ada karena kita mengejar kemajuan harian Jadi lebih baik dari kamu yang kemarin Coba kecilin target hingga terasa mudah dan lakukan setiap hari Ketsudan Soku Jikko - Keputusan Langsung Eksekusi Kesetiaan terhadap jadwal itu hukum Selalu di jam yang tetap dan tepat, tidak ada negosiasi Hilangkan negosiasi dengan dirimu sendiri Berhenti memanjakan dirimu sendiri Semakin kamu bernegosiasi dengan dirimu, akan semakin lemah dirimu Shugyo - Latihan Keras Latih dirimu untuk tidak kebanyakan drama Jangan punya mentalitas victim Kalau keras ke orang lain ujung-ujungnya malah jadi preman atau fanatic Kalau mau keras ke diri sendiri, tidak apa-apa karena tidak mengganggu orang lain Tidak usah merasa bosan saat melakukan keras ke diri sendiri Selalu konsisten setiap hari, apapun yang terjadi Pasti akan selalu ada rasa bosan dan ini yang akan menempamu Bushido - Kehormatan di atas segalanya Hormat lewat konsistensi Kehormatan lebih tinggi daripada kenyamanan Hormati dirimu sendiri dengan tidak bermalas-malasan Kalau kamu sendiri tidak bisa menghormati dirimu sendiri, pantaskah orang lain menghormatimu? Menepati janji ke diri sendiri Buat identitas diri sebagai orang yang konsisten Disiplin itu senjata dan bisa di gunakan berulang-ulang sampai tua\nDisiplin tidak seperti harta benda yang bisa di ambil, ini akan melekat ke dirimu",
+    "description": "Summary dari video Bennix yang berjudul 5 Rahasia Disiplin Samurai Jepang untuk Mengalahkan Malas!\nDisiplin lebih penting dari pada motivasi\nFokus membangun disiplin yang benar bukan menunggu semangat itu datang\nKalau menunggu motivasi kita akan kalah di medan perang, karena perang bisa datang kapan saja\n5 Prinsip dasar Samurai Kodo Ga Kokoro O Tsukuru (Tindakan membentuk perasaan) Bertindak tanpa mengandalkan mood Bertindak itu harus di lakukan karena itu memang harus dilakukan tanpa menunggu mood Dalam filosofi samurai, perasaan itu tidak boleh di andalkan dan di percaya Karena perasaan itu labil dan bisa berubah ubah (bisa naik turun) Kaizen - Lakukan sesuatu yang kecil tapi rutin setiap hari Lakukan perbaikan 1% terus menerus Seperti menabung, sedikit sedikit lama lama menjadi bukit (coumpounding) Progress itu ada karena kita mengejar kemajuan harian Jadi lebih baik dari kamu yang kemarin Coba kecilin target hingga terasa mudah dan lakukan setiap hari Ketsudan Soku Jikko - Keputusan Langsung Eksekusi Kesetiaan terhadap jadwal itu hukum Selalu di jam yang tetap dan tepat, tidak ada negosiasi Hilangkan negosiasi dengan dirimu sendiri Berhenti memanjakan dirimu sendiri Semakin kamu bernegosiasi dengan dirimu, akan semakin lemah dirimu Shugyo - Latihan Keras Latih dirimu untuk tidak kebanyakan drama Jangan punya mentalitas victim Kalau keras ke orang lain ujung-ujungnya malah jadi preman atau fanatic Kalau mau keras ke diri sendiri, tidak apa-apa karena tidak mengganggu orang lain Tidak usah merasa bosan saat melakukan keras ke diri sendiri Selalu konsisten setiap hari, apapun yang terjadi Pasti akan selalu ada rasa bosan dan ini yang akan menempamu Bushido - Kehormatan di atas segalanya Hormat lewat konsistensi Kehormatan lebih tinggi daripada kenyamanan Hormati dirimu sendiri dengan tidak bermalas-malasan Kalau kamu sendiri tidak bisa menghormati dirimu sendiri, pantaskah orang lain menghormatimu? Menepati janji ke diri sendiri Buat identitas diri sebagai orang yang konsisten Disiplin itu senjata dan bisa di gunakan berulang-ulang sampai tua",
+    "tags": [],
+    "title": "5 Rahasia Disiplin Samurai Jepang untuk Mengalahkan Malas!",
+    "uri": "/youtube/bennix/disiplinsamurai/index.html"
+  },
+  {
+    "breadcrumb": "Relearn Everything \u003e Youtubes",
+    "content": "Summary yang di buat dari channel Bennix",
+    "description": "Summary yang di buat dari channel Bennix",
+    "tags": [],
+    "title": "Bennix",
+    "uri": "/youtube/bennix/index.html"
   },
   {
     "breadcrumb": "Relearn Everything \u003e Trading",
